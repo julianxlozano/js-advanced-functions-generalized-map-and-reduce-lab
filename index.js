6baefe6ce325bc67b83f2e2998c4616036e2573a
julianxlozano <julianxlozano@gmail.com>
2021-06-01 16:31:29 -0500

    commit tree a58cc00c6267837dca603cfdc3b968b54506e03b

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Add your functions here
+
+
+
+
+function map(array,fn){
+    let newArray = [];
+    for (let i = 0;i<array.length;i++){
+       newArray.push(fn(array[i]))
+    };
+    return newArray
+};
+
+
+function reduce(array,fn,startingPoint = 0){
+
+    let isAllNums = array.every(function(n){return Number.isInteger(n)})
+
+
+    if (startingPoint === 0 && isAllNums === true){
+    let accumulator = startingPoint
+
+    for (let i = 0;i<array.length;i++){
+       accumulator = fn(array[i],accumulator)
+    };
+
+    return accumulator 
+    }else if (startingPoint === 0 && isAllNums === false){
+    
+        let accumulator = !startingPoint
+
+        for (let i = 0;i<array.length;i++){
+           accumulator = fn(array[i],accumulator)
+        };
+    
+        return accumulator 
+    
+    
+    
+    
+    }else{
+        let accumulator = startingPoint
+
+        for (let i = 0;i<array.length;i++){
+           accumulator = fn(array[i],accumulator)
+        };
+    
+        return accumulator
+        
+
+    };
+
+    
+};
